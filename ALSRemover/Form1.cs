e277023b8d6598e5ecac0746e649dfb8ec2eca38
zeroKilo <b_d_b@web.de>
2017-09-27 18:58:30 +0200

    initial commit
    initial commit

---
+++ ALSRemover/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ALSRemover
+{
+    public partial class Form1 : Form
+    {
+        byte[] mem;
+        string name;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {           
+            OpenFileDialog d = new OpenFileDialog();
+            d.Filter = "*.exe;*.dll|*.exe;*.dll";
+            if (d.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                button2.Enabled = false;
+                mem = File.ReadAllBytes(d.FileName);
+                int off = BitConverter.ToInt32(mem, 0x3C);
+                off += 0x5E;
+                int flags = BitConverter.ToInt32(mem, off);
+                if ((flags & 0x40) != 0)
+                {
+                    mem[off] &= 0xBF;
+                    button2.Enabled = true;
+                    name = Path.GetFileName(d.FileName);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog d = new SaveFileDialog();
+            d.Filter = "*.exe;*.dll|*.exe;*.dll";
+            d.FileName = name;
+            if (d.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                File.WriteAllBytes(d.FileName, mem);
+                MessageBox.Show("Done.");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ALSR Remover by Warranty Voider
+
+this tool removes the flag for ALSR in the PE header, so you can compare debugger breakpoints with the static analysis
